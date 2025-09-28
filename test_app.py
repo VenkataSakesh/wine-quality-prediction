a83e2b19b6e851599f865e9f821b592a47cfa9e4
VenkataSakesh <s224855437@deakin.edu.au>
2025-09-28 10:05:08 +1000

    feat: Add all project files

--- test_app.py
+++ test_app.py
@@ -9,6 +9,10 @@ def test_home():
     assert "Wine Quality" in response.json()["message"]
 
 def test_predict():
-    response = client.post("/predict", json=[13.0, 2.5, 2.3, 15.6, 120.0, 2.8, 2.6, 0.3, 1.56, 5.1, 1.04, 3.0, 800.0])
+    # Wrap the list in a dictionary with the "features" key
+    payload = {
+        "features": [13.0, 2.5, 2.3, 15.6, 120.0, 2.8, 2.6, 0.3, 1.56, 5.1, 1.04, 3.0, 800.0]
+    }
+    response = client.post("/predict", json=payload)
     assert response.status_code == 200
     assert "quality" in response.json()
